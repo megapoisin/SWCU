4d0ca06fbfb9414c01251e50bf9c6c331ac7a48e
Megapoisin <storchethan+github@gmail.com>
2024-02-03 07:47:39 -0500

    aaaaaaaaaa

---
+++ marked.js
@@ -0,0 +1,15 @@
+// marked.js
+
+/**
+ * Marked
+ * A markdown parser and compiler.
+ * Copyright (c) 2011-2021, Christopher Jeffrey (MIT License).
+ * https://github.com/markedjs/marked
+ */
+
+const marked = {
+    // Add your implementation of Marked here...
+  };
+  
+export default marked;
+  
